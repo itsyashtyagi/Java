2825e706f80749641fdd8b538871f3a327a8804c
Yash Tyagi <yashtyagi8006@gmail.com>
2024-11-12 22:17:55 +0530

    basics

---
+++ Basics/DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes 
+{
+    public static void main(String[] args) {
+         
+        // Primitive DataTypes
+        int mum1 = 1;
+        byte by = 127;
+        short sh = 129;
+        long l = 1245l;
+
+        float f = 5.6f;
+        double d = 5.6;
+        
+        char c = 'a'; 
+        
+        boolean b = true;
+
+    }
+}
+
---
+++ Basics/TypeConversion.java
@@ -0,0 +1,12 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        byte by = (byte)128; 
+        byte b = 123;
+        int a = b;
+        int c = 23;
+        byte byt = (byte)c;
+        System.out.println(byt);
+        System.out.println(a);
+        System.out.println(by);
+    }
+}
